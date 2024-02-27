ca1b2ec4697a61a545bf361a7e9174e3b1531844
Aidan Aedy <aidanaedy@gmail.com>
2024-02-27 11:34:14 +0000

    Update Main.py to remove Lambda, need to add classes and adapt with exit etc.

--- Main.py
+++ Main.py
@@ -4,12 +4,16 @@ from tkinter import *
 window = Tk()
 window.title("Aidan's UI Window")
 
-# Add a label widget
+# Define functions for the button options
+def button_click():
+  print("Button Clicked!")
+
+# Add a label
 label = Label(window, text="Hello, Aidan!")
 label.pack()
 
-# Add a button widget
-button = Button(window, text="Click Me!", command=lambda: label.config(text="Clicked!"))
+# Add a button with a defined function
+button = Button(window, text="Click Me!", command=button_click)
 button.pack()
 
 # Start the event loop
